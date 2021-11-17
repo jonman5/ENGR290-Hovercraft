f4b626c56c420cce27afc790d97b80f891a12f19
Jonathan Perlman <jonathan.perlman@mail.concordia.ca>
2021-11-17 16:45:35 -0500

    Create range_sensors_test.ino
    range_sensors_test.ino enables reading the input from a range sensor

---
+++ range_sensors_test/range_sensors_test.ino
@@ -0,0 +1,18 @@
+long range_sensor1, mm;
+int output;
+void setup() {
+  Serial.begin(9600);
+  pinMode(PD3, INPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  
+  range_sensor1 = pulseIn(PD3, HIGH);
+  mm = range_sensor1;
+  Serial.println(mm);
+  output = (mm>1100)? 1:0;
+  Serial.println(output);
+  digitalWrite(12, output);
+  delay(100);
+}
