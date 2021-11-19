5f51de60b15a502e2fd84f8798022bc9a2349d59
Jonathan Perlman <jonnam2@gmail.com>
2021-11-19 10:40:41 -0500

    Update range_sensors_test.ino
    Take readings from both sensors

--- range_sensors_test/range_sensors_test.ino
+++ range_sensors_test/range_sensors_test.ino
@@ -1,21 +1,29 @@
-long range_sensor1, mm;
-int output;
+long range_sensor1, mm1, range_sensor2, mm2 ;
+int output, threshold, distance1, distance2;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PD3, INPUT); //PD3 
+  pinMode(PD3, INPUT); //PD3
   pinMode(12, OUTPUT);
-  pinMode(PD3, INPUT); //PD4
+  pinMode(PD4, INPUT); //PD4
   pinMode(12, OUTPUT);
 }
 
 void loop() {
-  
+  //Detect and print distance from range sensor 1
   range_sensor1 = pulseIn(PD3, HIGH);
-  mm = range_sensor1;
-  Serial.println(mm);
-  output = (mm>1100)? 1:0;
-  Serial.println(output);
+  mm1 = range_sensor1;
+  distance1 = mm1/100; //distance1= distance measured by range sensor 1 in cm
+  Serial.println("Sensor 1 distance: " + distance1);
+
+  //Detect and print distance from range sensor 2
+  range_sensor2 = pulseIn(PD4, HIGH);
+  mm2 = range_sensor2;
+  distance2 = mm2/100; //distance2= distance measured by range sensor 2 in cm
+  Serial.println("Sensor 2 distance: " + distance2);
+
+  //If distance 1 or distance 2 is less than the threshold distance, turn on LED
+  output = ((distance1<threshold)|(distance2<threshold))? 1:0;
   digitalWrite(12, output);
   delay(100);
 }
